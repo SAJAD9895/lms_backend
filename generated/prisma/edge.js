e5fa43b1f8bdba89c4c046245a02c88e29f721ea
sajad <sajadmji21@gmail.com>
2025-05-01 13:33:35 +0530

    add rds new and

--- generated/prisma/edge.js
+++ generated/prisma/edge.js
@@ -219,12 +219,11 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": null
+        "value": "postgresql://postgres:UwfKUthXcTKLnODnmVmfnkPvGxgOTVxt@nozomi.proxy.rlwy.net:30043/railway"
       }
     }
   },
---
+++ readme.txt
@@ -0,0 +1,9 @@
+
+// rds change type this
+npx prisma migrate dev --name init
+npx prisma db push
+
+
+
+//run code
+node src/index.js
