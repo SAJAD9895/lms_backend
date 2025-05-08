81420ae0a72b038be0258544d444d0de65ea704d
sajad <sajadmji21@gmail.com>
2025-05-08 11:37:22 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const { ApolloServer } = require('apollo-server-express');
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 const cors = require('cors');
+const { ApolloServerPluginLandingPageLocalDefault } = require('apollo-server-core');
 
 const app = express();
 app.use(cors({
@@ -14,22 +15,18 @@ async function startServer() {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    // playground: true,
     introspection: true, // allows schema introspection
-    // plugins: [
-    //   require('apollo-server-core').ApolloServerPluginLandingPageGraphQLPlayground({
-    //     settings: {
-    //       'request.credentials': 'include',
-    //     },
-    //   }),
-    // ],
+    plugins: [
+      ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+    ],
   });
 
   await server.start(); // Required for Apollo Server v3+
-  server.applyMiddleware({ app });
+  server.applyMiddleware({ app, path: '/graphql' });
 
-  app.listen(4000, () => {
-    console.log('🚀 Server ready at http://localhost:4000/graphql');
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => {
+    console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
   });
 }
 
